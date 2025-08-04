a422a25d39e9e8899cf639c048e0c5d31fe5defd
josiah <josiah.chapman@gmail.com>
2025-08-05 00:06:23 +0900

    (~)

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 installation_requirements = [
-    "openai-agents==0.0.16",
+    "openai-agents==0.2.4",
     "loguru==0.7.3",
     "neo4j==5.28.1",
     "google-genai==1.12.1"
 ]
 
 setup(
-    version="0.17",
+    version="0.18",
     name="freeflock-contraptions",
     description="A collection of contraptions",
     author="(~)",
